8593e960f8334f0ace0751867eb076c6d4d2db12
Allan Ikeda <allanikeda@outlook.com>
2023-08-19 10:03:09 -0300

    tests - pytest, doctests...

---
+++ 02-testes-automatizados/example2.py
@@ -0,0 +1,11 @@
+def sum_two_numbers(a, b):
+    """Retorna a soma de dois números recebidos por parâmetro.
+
+    Exemplos
+    --------
+    >>> sum_two_numbers(0, 0)
+    0
+    >>> sum_two_numbers(1, 1)
+    2
+    """
+    return a + b
---
+++ 02-testes-automatizados/tests/a_test.py
@@ -0,0 +1,5 @@
+def test_a_simple_test():
+    assert True
+
+def test_sum(my_list):
+    assert sum(my_list) == 6
