9fcce7bbdba7ce6f19dc63cde05de4cc8e5a8d11
Allan Ikeda <allanikeda@outlook.com>
2023-08-19 14:32:28 -0300

    tests - pytest, Faker, Config debugger...

---
+++ 03-avançando-nos-testes/tests/test_faker.py
@@ -0,0 +1,10 @@
+from faker import Faker
+
+
+faker = Faker(locale='pt_BR')
+
+def test_faker_email(faker):
+    fake_email = faker.email()
+    assert isinstance(fake_email, str)
+    assert '@' in fake_email
+    assert '.' in fake_email
